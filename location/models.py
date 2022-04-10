7df71046f89ea748ecdd0b4d7c53b1a8b633f741
kiyimba1 <edwineddie54@gmail.com>
2022-04-10 15:04:35 +0300

    first commit

---
+++ location/models.py
@@ -0,0 +1,13 @@
+from unicodedata import name
+from django.contrib.gis.db import models
+
+# Create your models here.
+class Hotel(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    loction = models.PointField(null=True)
+
+    def __str__(self) -> str:
+        return self.name
---
+++ location/views.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from .models import Hotel
+
+from django.contrib.gis.geos import Point
+from geopy.geocoders import Nominatim
+
+# Create your views here.
